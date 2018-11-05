9eefbb6fe0763945180218c19e2cde87a0daa211
Elvis Wang <wangbo12@58ganji.com>
2018-11-05 10:03:01 +0800

    doc(wtable): add doc for WtableStub

--- wtable/core/src/main/java/im/wangbo/bj58/wtable/core/WtableException.java
+++ wtable/core/src/main/java/im/wangbo/bj58/wtable/core/WtableException.java
@@ -1,4 +1,53 @@
 package im.wangbo.bj58.wtable.core;
 
+import javax.annotation.Nullable;
+
 public class WtableException extends Exception {
+    private String op;
+    private Table table;
+
+    @Nullable
+    private RowKey rowKey;
+    @Nullable
+    private ColKey colKey;
+    @Nullable
+    private Value value;
+    @Nullable
+    private CasStamp casStamp;
+
+    public WtableException(
+            final String op,
+            @Nullable final Table table,
+            final Throwable cause
+    ) {
+        this(op, table, null, null, cause);
+    }
+
+    public WtableException(
+            final String op,
+            @Nullable final Table table,
+            @Nullable final RowKey rowKey,
+            @Nullable final ColKey colKey,
+            final Throwable cause
+    ) {
+        this(op, table, rowKey, colKey, null, null, cause);
+    }
+
+    public WtableException(
+            final String op,
+            @Nullable final Table table,
+            @Nullable final RowKey rowKey,
+            @Nullable final ColKey colKey,
+            @Nullable final Value value,
+            @Nullable final CasStamp casStamp,
+            final Throwable cause
+    ) {
+        super("Failed to perform wtable operation " + op, cause);
+        this.op = op;
+        this.table = table;
+        this.rowKey = rowKey;
+        this.colKey = colKey;
+        this.value = value;
+        this.casStamp = casStamp;
+    }
 }
--- wtable/core/src/main/java/im/wangbo/bj58/wtable/repository/AbstractRepositoryImpl.java
+++ wtable/core/src/main/java/im/wangbo/bj58/wtable/repository/AbstractRepositoryImpl.java
@@ -38,73 +38,36 @@ abstract class AbstractRepositoryImpl implements Repository {
     ) throws WtableException;
 
     @Override
-    public Optional<Value> find(final RowKey r, final ColKey c) {
-        try {
-            return findByKey(wtable, table, r, c);
-        } catch (WtableException ex) {
-            // TODO
-            return Optional.empty();
-        }
+    public Optional<Value> find(final RowKey r, final ColKey c) throws WtableException {
+        return findByKey(wtable, table, r, c);
     }
 
     @Override
-    public void overrideInsert(final RowKey r, final ColKey c, final Value val) {
-        try {
-            wtable.set(table, r, c, val);
-        } catch (WtableException ex) {
-            // TODO
-        }
+    public void overrideInsert(final RowKey r, final ColKey c, final Value val) throws WtableException {
+        wtable.set(table, r, c, val);
     }
 
     @Override
-    public void delete(final RowKey r, final ColKey c) {
-        try {
-            wtable.delete(table, r, c);
-        } catch (WtableException ex) {
-            // TODO
-        }
+    public void delete(final RowKey r, final ColKey c) throws WtableException {
+        wtable.delete(table, r, c);
     }
 
     @Override
-    public boolean insertOnNotExists(final RowKey r, final ColKey c, final Value val) {
-        final CasLockedValue casLocked;
-        try {
-            casLocked = wtable.getCasLocked(table, r, c);
-        } catch (WtableException ex) {
-            // TODO
-            throw new RuntimeException(ex);
-        }
-
+    public boolean insertOnNotExists(final RowKey r, final ColKey c, final Value val) throws WtableException {
+        final CasLockedValue casLocked = wtable.getCasLocked(table, r, c);
         if (casLocked.value().isPresent()) {
             return false;
         } else {
-            try {
-                wtable.set(table, r, c, val, casLocked.casStamp());
-            } catch (WtableException ex) {
-                // TODO
-            }
-
+            wtable.set(table, r, c, val, casLocked.casStamp());
             return true;
         }
     }
 
     @Override
-    public boolean updateOnExists(final RowKey r, final ColKey c, final Value val) {
-        final CasLockedValue casLocked;
-        try {
-            casLocked = wtable.getCasLocked(table, r, c);
-        } catch (WtableException ex) {
-            // TODO
-            throw new RuntimeException(ex);
-        }
-
+    public boolean updateOnExists(final RowKey r, final ColKey c, final Value val) throws WtableException {
+        final CasLockedValue casLocked = wtable.getCasLocked(table, r, c);
         if (casLocked.value().isPresent()) {
-            try {
-                wtable.set(table, r, c, val, casLocked.casStamp());
-            } catch (WtableException ex) {
-                // TODO
-            }
-
+            wtable.set(table, r, c, val, casLocked.casStamp());
             return true;
         } else {
             return false;
@@ -113,24 +76,13 @@ abstract class AbstractRepositoryImpl implements Repository {
 
     @Override
     public boolean compareAndUpdate(
-            final RowKey r, final ColKey c, final Function<Value, Optional<Value>> updater
-    ) {
-        final CasLockedValue casLocked;
-        try {
-            casLocked = wtable.getCasLocked(table, r, c);
-        } catch (WtableException ex) {
-            // TODO
-            throw new RuntimeException(ex);
-        }
+            final RowKey r, final ColKey c, final Function<Value, Value updater
+    ) throws WtableException {
+        final CasLockedValue casLocked = wtable.getCasLocked(table, r, c);
 
-        final Optional<Value> newVal = casLocked.value().flatMap(updater);
+        final Optional<Value> newVal = casLocked.value().map(updater);
         if (newVal.isPresent()) {
-            try {
-                wtable.set(table, r, c, newVal.get(), casLocked.casStamp());
-            } catch (WtableException ex) {
-                // TODO
-            }
-
+            wtable.set(table, r, c, newVal.get(), casLocked.casStamp());
             return true;
         } else {
             return false;
@@ -140,23 +92,12 @@ abstract class AbstractRepositoryImpl implements Repository {
     @Override
     public boolean compareAndDelete(
             final RowKey r, final ColKey c, final Predicate<Value> when
-    ) {
-        final CasLockedValue casLocked;
-        try {
-            casLocked = wtable.getCasLocked(table, r, c);
-        } catch (WtableException ex) {
-            // TODO
-            throw new RuntimeException(ex);
-        }
+    ) throws WtableException {
+        final CasLockedValue casLocked = wtable.getCasLocked(table, r, c);
 
         final Optional<Value> newVal = casLocked.value().filter(when);
         if (newVal.isPresent()) {
-            try {
-                wtable.delete(table, r, c, casLocked.casStamp());
-            } catch (WtableException ex) {
-                // TODO
-            }
-
+            wtable.delete(table, r, c, casLocked.casStamp());
             return true;
         } else {
             return false;
