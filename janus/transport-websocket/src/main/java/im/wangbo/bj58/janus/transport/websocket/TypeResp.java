e9b9c48b21c64ba4e52ba584db58c797114241eb
Elvis Wang <wangbo12@58ganji.com>
2018-11-06 09:23:00 +0800

    refactor(janus): clean out unused code

--- janus/transport-websocket/src/main/java/im/wangbo/bj58/janus/transport/websocket/TypeResp.java
+++ janus/transport-websocket/src/main/java/im/wangbo/bj58/janus/transport/websocket/TypeResp.java
@@ -1,9 +1,5 @@
 package im.wangbo.bj58.janus.transport.websocket;
 
-import com.google.auto.value.AutoValue;
-
-import javax.json.JsonObject;
-
 /**
  * TODO add brief description here
  *
@@ -11,25 +7,16 @@ import javax.json.JsonObject;
  *
  * @author Elvis Wang [wangbo12 -AT- 58ganji -DOT- com]
  */
-@AutoValue
-abstract class TypeResp {
-    static final String TYPE_ACK           = "ack";
-    static final String TYPE_ERROR         = "error";
-    static final String TYPE_EVENT         = "event";
-    static final String TYPE_HANGUP        = "hangup";
-    static final String TYPE_MEDIA         = "media";
-    static final String TYPE_SERVER_INFO   = "server_info";
-    static final String TYPE_SLOWLINK      = "slowlink";
-    static final String TYPE_SUCCESS       = "success";
-    static final String TYPE_WEBRTCUP      = "webrtcup";
+final class TypeResp {
+    private TypeResp() { throw new AssertionError("Construction forbidden"); }
 
-    abstract String getResponseType();
-
-    static TypeResp create(String type) {
-        return new AutoValue_TypeResp(type);
-    }
-
-    static TypeResp fromJson(final JsonObject obj) {
-        return TypeResp.create(obj.getString("janus"));
-    }
+    static final String TYPE_ACK = "ack";
+    static final String TYPE_ERROR = "error";
+    static final String TYPE_EVENT = "event";
+    static final String TYPE_HANGUP = "hangup";
+    static final String TYPE_MEDIA = "media";
+    static final String TYPE_SERVER_INFO = "server_info";
+    static final String TYPE_SLOWLINK = "slowlink";
+    static final String TYPE_SUCCESS = "success";
+    static final String TYPE_WEBRTCUP = "webrtcup";
 }
