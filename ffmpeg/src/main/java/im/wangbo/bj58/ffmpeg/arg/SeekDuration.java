dc74bfd669f8b546a9e3f212962852758becf77f
Elvis Wang <wangbo12@58ganji.com>
2019-06-11 17:07:47 +0800

    Add SeekPosition and SeekDuration

---
+++ ffmpeg/src/main/java/im/wangbo/bj58/ffmpeg/arg/SeekDuration.java
@@ -0,0 +1,29 @@
+package im.wangbo.bj58.ffmpeg.arg;
+
+import com.google.auto.value.AutoValue;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * TODO add brief description here
+ *
+ * @author Elvis Wang
+ */
+@AutoValue
+public abstract class SeekDuration implements Value {
+    abstract Duration duration();
+
+    @Override
+    public final String asString() {
+        return duration().getSeconds() + "." + (duration().getNano() / 1000_000);
+    }
+
+    public static SeekDuration of(final Duration duration) {
+        return new AutoValue_SeekDuration(duration);
+    }
+
+    public static SeekDuration of(final long duration, final TimeUnit unit) {
+        return of(Duration.ofNanos(unit.toNanos(duration)));
+    }
+}
--- ffmpeg/src/main/java/im/wangbo/bj58/ffmpeg/arg/Value.java
+++ ffmpeg/src/main/java/im/wangbo/bj58/ffmpeg/arg/Value.java
@@ -1,11 +1,5 @@
 package im.wangbo.bj58.ffmpeg.arg;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.concurrent.TimeUnit;
-
 /**
  * TODO add brief description here
  *
@@ -26,19 +20,7 @@ public interface Value {
         return of(String.valueOf(l));
     }
 
-    static Value duration(final long duration, final TimeUnit unit) {
-        return duration(Duration.ofNanos(unit.toNanos(duration)));
-    }
-
-    static Value duration(final Duration duration) {
-        return ofString(duration.getSeconds() + "." + (duration.getNano() / 1000_000));
-    }
-
-    static Value datetime(final OffsetDateTime t) {
-        return ofString(DateTimeFormatter.ISO_INSTANT.format(t));
-    }
-
-    static Value datetime(final LocalDateTime t) {
-        return ofString(DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(t));
+    static Value ofInt(final int l) {
+        return of(String.valueOf(l));
     }
 }
