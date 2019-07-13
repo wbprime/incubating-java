792bc311651f9be58ac0876e337a57170a4aabc3
Elvis Wang <wangbo12@58ganji.com>
2019-07-13 14:14:11 +0800

    Remove StdValue

--- ffmpeg/src/main/java/im/wangbo/bj58/ffmpeg/common/Value.java
+++ ffmpeg/src/main/java/im/wangbo/bj58/ffmpeg/common/Value.java
@@ -13,20 +13,4 @@ public interface Value {
     default Optional<String> stringify() {
         return Optional.of(asString());
     }
-
-    static Value of(final String value) {
-        return StdValue.of(value);
-    }
-
-    static Value ofString(final String str) {
-        return of(str);
-    }
-
-    static Value ofLong(final long l) {
-        return of(String.valueOf(l));
-    }
-
-    static Value ofInt(final int l) {
-        return of(String.valueOf(l));
-    }
 }
